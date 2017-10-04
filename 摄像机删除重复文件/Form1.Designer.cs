f1f1d644e8f01ea639d22bf80dfb68ae028e0af2
siyuan xue <siyuan xue>
2017-10-04 21:45:10 +0800

    添加项目文件。

---
+++ 摄像机删除重复文件/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace 摄像机删除重复文件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.listBoxDirectory = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(400, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "文件夹";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(614, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "删除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(511, 10);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 5;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 12;
+            this.listBoxFiles.Location = new System.Drawing.Point(400, 41);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(543, 364);
+            this.listBoxFiles.TabIndex = 6;
+            // 
+            // listBoxDirectory
+            // 
+            this.listBoxDirectory.FormattingEnabled = true;
+            this.listBoxDirectory.ItemHeight = 12;
+            this.listBoxDirectory.Location = new System.Drawing.Point(68, 39);
+            this.listBoxDirectory.Name = "listBoxDirectory";
+            this.listBoxDirectory.Size = new System.Drawing.Size(317, 364);
+            this.listBoxDirectory.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 413);
+            this.Controls.Add(this.listBoxDirectory);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "删除摄像机重复文件";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.ListBox listBoxDirectory;
+    }
+}
+
---
+++ 摄像机删除重复文件/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 摄像机删除重复文件
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> dics;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件夹";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                if (!string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    textBox1.Text = dialog.SelectedPath;
+                }
+                else
+                {
+                    MessageBox.Show("请选择一个文件夹", "提示");
+                }
+
+            }
+        }
+        /// <summary>
+        /// 目录变更事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string path = textBox1.Text;
+            if (Directory.Exists(path)) //目录存在
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                listBoxDirectory.Items.Clear();
+                foreach (DirectoryInfo item in dirs)
+                {
+                    listBoxDirectory.Items.Add(item.Name);
+                }
+            }
+
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox1.Text))
+            {
+                DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+
+                dics = FindFiles(dir);
+                listBoxFiles.Items.Clear();
+                foreach (var item in dics.Keys)
+                {
+                    listBoxFiles.Items.Add(dics[item]);
+                }
+            }
+        }
+        /// <summary>
+        /// 递归查找文件
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <returns></returns>
+        private Dictionary<string, string> FindFiles(DirectoryInfo dir)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            FileInfo[] files = dir.GetFiles();
+            if (files.Count() > 0)
+            {
+                foreach (var item in files)
+                {
+                    //判断文件名称是否包含（）;
+                    if (item.Name.Contains('(')&&item.Name.Contains(')'))
+                    {
+                        result.Add(item.FullName, item.Name);
+                    }
+                    
+                }
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            if (dirs.Count() > 0)
+            {
+                foreach (var item in dirs)
+                {
+                    Dictionary<string, string> dicTemp = new Dictionary<string, string>();
+                    dicTemp = FindFiles(item); 
+                    if (dicTemp!=null&&dicTemp.Count>0)
+                    {
+                        //dics= dics.Concat(dicTemp) as Dictionary<string, string>;
+                        foreach (var key in dicTemp.Keys)
+                        {
+                            result.Add(key, dicTemp[key]);
+                        }
+                    }
+                 
+                }
+            }
+
+            return result;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            if (dics.Count()>0)
+            {
+                foreach (string key in dics.Keys)
+                {
+                    try
+                    {
+                        File.Delete(key);
+                        count++;
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("删除文件失败!"+key,"提示");
+                        throw;
+                    }
+                    
+                }
+                MessageBox.Show("成功删除"+count+"个文件！");
+            }
+        }
+    }
+}
